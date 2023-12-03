7015eaa95463b50fae12413e15acb7721f2c17b5
randomwrites <150580195+randomwrites@users.noreply.github.com>
2023-12-02 22:05:03 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener('push', event => {
+    const options = {
+        body: event.data.text(),
+        icon: 'icon.png',
+    };
+
+    event.waitUntil(
+        self.registration.showNotification('Push Notification', options)
+    );
+});
